23b3f8cb1d91cfad853cdc2f84756d8012dad40f
Matthew Blanchard <matthewblanc@msn.com>
2021-03-01 21:03:16 -0500

    Changed some constants.

--- src/bezier/offset.rs
+++ src/bezier/offset.rs
@@ -100,7 +100,7 @@ fn subdivide_offset<'a, CurveIn, CurveOut>(curve: &CurveSection<'a, CurveIn>, in
 where   CurveIn:        NormalCurve+BezierCurve,
         CurveOut:       BezierCurveFactory<Point=CurveIn::Point>,
         CurveIn::Point: Coordinate2D+Normalize {
-    const MAX_DEPTH: usize = 5;
+    const MAX_DEPTH: usize = 10;
 
     // Fetch the original points
     let start               = curve.start_point();
@@ -119,6 +119,9 @@ where   CurveIn:        NormalCurve+BezierCurve,
         if characterize_curve(curve) != CurveCategory::Linear && depth < MAX_DEPTH {
             // Collinear normals
             let divide_point    = 0.5;
+
+            //let param = ArcLengthParameterization::from(curve);
+            //let divide_point = param.parameterize(0.5);
 
             let mid_offset      = initial_offset + (final_offset - initial_offset) * divide_point;
             let left_curve      = curve.subsection(0.0, divide_point);
@@ -145,6 +148,9 @@ where   CurveIn:        NormalCurve+BezierCurve,
         if distance_ratio < 0.9 && depth < MAX_DEPTH {
             let divide_point    = 0.5;
 
+            //let param = ArcLengthParameterization::from(curve);
+            //let divide_point = param.parameterize(0.5);
+
             let mid_offset      = initial_offset + (final_offset - initial_offset) * divide_point;
             let left_curve      = curve.subsection(0.0, divide_point);
             let right_curve     = curve.subsection(divide_point, 1.0);
@@ -156,7 +162,7 @@ where   CurveIn:        NormalCurve+BezierCurve,
                 .chain(right_offset)
                 .collect()
         } else {
-            // Event intersection point
+            // Event intersection point`
             smallvec![offset_by_scaling(curve, initial_offset, final_offset, intersect_point, normal_start, normal_end)]
         }
 
@@ -185,8 +191,12 @@ where   CurveIn:        NormalCurve+BezierCurve,
     let start_scale = (intersect_point.distance_to(&new_start))/(intersect_point.distance_to(&start));
     let end_scale   = (intersect_point.distance_to(&new_end))/(intersect_point.distance_to(&end));
 
-    let new_cp1     = ((cp1-intersect_point) * start_scale) + intersect_point;
-    let new_cp2     = ((cp2-intersect_point) * end_scale) + intersect_point;
+    // When the scale is changing, the control points are effectively 1/3rd and 2/3rds of the way along the curve
+    let cp1_scale   = (end_scale - start_scale) * (1.0/3.0) + start_scale;
+    let cp2_scale   = (end_scale - start_scale) * (2.0/3.0) + start_scale;
+
+    let new_cp1     = ((cp1-intersect_point) * cp1_scale) + intersect_point;
+    let new_cp2     = ((cp2-intersect_point) * cp2_scale) + intersect_point;
 
     CurveOut::from_points(new_start, (new_cp1, new_cp2), new_end)
 }
@@ -210,4 +220,103 @@ where   CurveIn:        NormalCurve+BezierCurve,
     let new_end     = end + (unit_normal_end * final_offset);
 
     CurveOut::from_points(new_start, (new_cp1, new_cp2), new_end)
+}
+
+// We build a table of total arc length along the line and use it to map 0-1
+// to the arclength of the curve such that 0.5 is halfway along the curve by arc-length
+pub struct ArcLengthParameterization
+{
+    pub arclens: Vec<f64>
+}
+
+impl ArcLengthParameterization
+{
+    pub fn from<'a, CurveIn>(evaluable: &CurveSection<'a, CurveIn>) -> Self
+        where   CurveIn: NormalCurve+BezierCurve,
+        CurveIn::Point: Coordinate2D+Normalize 
+    {
+        let mut output = Vec::new();
+        // TODO: this is an arbitrary number and should be replaced with something more robust
+        // TODO: preferably a tolerance value
+        let arclen_cuts = 200;
+        let max_cuts = 200 + 1;
+
+        let mut prev_point = evaluable.point_at_pos(0.0);
+        let mut sum = 0.0;
+        output.push(sum);
+        
+        let mut i = 1;
+        while i < max_cuts
+        {
+            let t = i as f64 / arclen_cuts as f64;
+            let point = evaluable.point_at_pos(t);
+            let dist = point.distance_to(&prev_point);
+            sum = sum + dist;
+            output.push(sum);
+
+            prev_point = point;
+            i = i + 1;
+        }
+
+        return Self {
+            arclens: output
+        }
+    }
+
+    pub fn get_total_arclen(&self) -> f64
+    {
+        return *self.arclens.last().unwrap();
+    }
+
+    // Have to implement a custom binary search here because we're looking
+    // not for an exact index but the index of the highest value that's less than
+    // the target
+    fn search_for_index(&self, target: f64) -> usize
+    {
+        let mut left = 0;
+        let mut right = self.arclens.len() - 1;
+
+        while left < right {
+            let middle = (right+left)/2;
+
+            if left == middle { return middle; }
+            if right == middle { return left; }
+            if self.arclens[middle] == target { return middle };
+
+            if self.arclens[middle] < target {
+                left = middle
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+
+        // This needs to be replaced with success/failure.
+        panic!("Couldn't find the target arc length!")
+    }
+}
+
+impl ArcLengthParameterization
+{
+    fn parameterize(&self, u: f64) -> f64
+    {
+        let target_arclen = u * self.arclens[self.arclens.len() - 1];
+ 
+        let arclen_index = self.search_for_index(target_arclen);
+        if target_arclen == self.arclens[arclen_index]
+        {
+           return arclen_index as f64 / (self.arclens.len() - 1) as f64;
+        }
+        else
+        {
+            let len_start = self.arclens[arclen_index];
+            let len_end = self.arclens[arclen_index+1];
+            let segment_len = len_end - len_start;
+
+            let segment_fraction = (target_arclen - len_start) / segment_len;
+
+            return (arclen_index as f64 + segment_fraction) / (self.arclens.len() - 1) as f64;
+        }
+    }
 }
